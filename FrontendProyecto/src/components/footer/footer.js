8ce3d6d00251f0112876920f87f7991031df5bf4
Jhenox86 <jhenox@gmail.com>
2022-11-21 22:37:05 -0500

    Enlaces Tienda y home con plantilla base

---
+++ FrontendProyecto/src/components/footer/footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    
+    render() { 
+        return ( 
+
+            <Container>
+                
+            
+            </Container>
+         );
+    }
+}
+ 
+ 
---
+++ FrontendProyecto/src/components/tienda/tienda.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+export default class Tienda extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+            
+           <h1 margin-top='150px'>SINTIENDA</h1>
+          
+        </Row>
+      </Container>
+    );
+  }
+}
